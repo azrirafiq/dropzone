a76d440022d818ae99eaa2d97361b721f7807b6a
azrirafiq_ <azrirafiq96@gmail.com>
2017-04-29 14:12:29 +0800

    edit file and add function

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-primary">
-                <div class="panel-heading">Create Products</div>
+                <div class="panel-heading">Edit Products</div>
 
                 <div class="panel-body">
                     @if($errors->all())
